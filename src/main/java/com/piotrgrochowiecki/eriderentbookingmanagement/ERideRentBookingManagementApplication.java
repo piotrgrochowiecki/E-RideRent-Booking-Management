7988b28ef1cb7e6883038212c61d8c8cda5037a5
Piotr Grochowiecki <Piotr.Grochowiecki@dxc.com>
2023-10-21 17:05:13 +0200

    Initial commit

---
+++ src/main/java/com/piotrgrochowiecki/eriderentbookingmanagement/ERideRentBookingManagementApplication.java
@@ -0,0 +1,13 @@
+package com.piotrgrochowiecki.eriderentbookingmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ERideRentBookingManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ERideRentBookingManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/piotrgrochowiecki/eriderentbookingmanagement/ERideRentBookingManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piotrgrochowiecki.eriderentbookingmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ERideRentBookingManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
