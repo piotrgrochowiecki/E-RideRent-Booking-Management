a301ce7a09b364b954f6906b9b6fc14016463ade
piotrgrochowiecki <piotrgrochowiecki@gmail.com>
2023-12-17 17:54:06 +0100

    added tests of getAll method of BookingService

--- src/test/java/com/piotrgrochowiecki/eriderentbookingmanagement/domain/BookingServiceTest.java
+++ src/test/java/com/piotrgrochowiecki/eriderentbookingmanagement/domain/BookingServiceTest.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.awt.print.Book;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -27,7 +29,7 @@ class BookingServiceTest {
 
     @Test
     @DisplayName("When getById is invoked" +
-                 "it should return one booking object")
+                 "then return one booking object")
     void shouldReturnOneBooking() {
         //given
         Booking booking1 = Booking.builder()
@@ -50,7 +52,7 @@ class BookingServiceTest {
 
     @Test
     @DisplayName("When getById is invoked with an id that does not exist" +
-                 "it should throw NotFoundRuntimeException")
+                 "then throw NotFoundRuntimeException")
     void shouldThrowNotFoundException() {
         //given
         when(bookingRepository.findById(2L))
@@ -60,6 +62,41 @@ class BookingServiceTest {
         assertThrows(NotFoundRuntimeException.class, () -> bookingService.getById(2L));
     }
 
+    @Test
+    @DisplayName("Given 2 booking objects, " +
+                 "when findAll is invoked, " +
+                 "then return list of two Booking objects")
+    void shouldReturnListOfTwoBookingObjects() {
+        //given
+        Booking booking1 = Booking.builder()
+                .id(1L)
+                .startDate(LocalDate.of(2023, 9, 28))
+                .endDate(LocalDate.of(2023, 10, 10))
+                .userUuid("userUuid1")
+                .carUuid("carUuid1")
+                .build();
 
+        Booking booking2 = Booking.builder()
+                .id(2L)
+                .startDate(LocalDate.of(2023, 10, 12))
+                .endDate(LocalDate.of(2023, 10, 18))
+                .userUuid("userUuid2")
+                .carUuid("carUuid2")
+                .build();
+
+        List<Booking> bookingList = List.of(booking1,
+                                            booking2);
+
+        when(bookingRepository.findAll())
+                .thenReturn(bookingList);
+
+        //when
+        List<Booking> result = bookingService.getAll();
+
+        //then
+        assertTrue(result.contains(booking1));
+        assertTrue(result.contains(booking2));
+        assertEquals(2, result.size());
+    }
 
 }
