8bc0cae60daf7bfe45d71b590b7f4b3b5fce1009
piotrgrochowiecki <piotrgrochowiecki@gmail.com>
2023-12-17 17:35:23 +0100

    added tests for getById method of BookingService

--- src/test/java/com/piotrgrochowiecki/eriderentbookingmanagement/domain/BookingServiceTest.java
+++ src/test/java/com/piotrgrochowiecki/eriderentbookingmanagement/domain/BookingServiceTest.java
@@ -1,13 +1,16 @@
 package com.piotrgrochowiecki.eriderentbookingmanagement.domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class BookingServiceTest {
@@ -23,15 +26,40 @@ class BookingServiceTest {
     private BookingRepository bookingRepository;
 
     @Test
-    void test1() {
+    @DisplayName("When getById is invoked" +
+                 "it should return one booking object")
+    void shouldReturnOneBooking() {
         //given
-        LocalDate newBookingStartDate = LocalDate.of(2023, 10, 6);
-        LocalDate newBookingEndDate = LocalDate.of(2023, 10, 18);
-
         Booking booking1 = Booking.builder()
+                .id(1L)
                 .startDate(LocalDate.of(2023, 9, 28))
                 .endDate(LocalDate.of(2023, 10, 10))
+                .userUuid("userUuid1")
+                .carUuid("carUuid1")
                 .build();
+
+        when(bookingRepository.findById(1L))
+                .thenReturn(Optional.of(booking1));
+
+        //when
+        Booking result = bookingService.getById(1L);
+
+        //then
+        assertEquals(result, booking1);
     }
+
+    @Test
+    @DisplayName("When getById is invoked with an id that does not exist" +
+                 "it should throw NotFoundRuntimeException")
+    void shouldThrowNotFoundException() {
+        //given
+        when(bookingRepository.findById(2L))
+                .thenReturn(Optional.empty());
+
+        //when and then
+        assertThrows(NotFoundRuntimeException.class, () -> bookingService.getById(2L));
+    }
+
+
 
 }
