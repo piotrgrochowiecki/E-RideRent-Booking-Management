86a60fe7d91230f0a018620e477de9de42d5c56c
piotrgrochowiecki <piotrgrochowiecki@gmail.com>
2023-12-18 19:20:52 +0100

    added tests of getAllBookingsOverlappingWithDates method of BookingService

--- src/test/java/com/piotrgrochowiecki/eriderentbookingmanagement/domain/BookingServiceTest.java
+++ src/test/java/com/piotrgrochowiecki/eriderentbookingmanagement/domain/BookingServiceTest.java
@@ -6,10 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-import java.awt.print.Book;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
@@ -92,6 +92,274 @@ class BookingServiceTest {
 
         //when
         List<Booking> result = bookingService.getAll();
+
+        //then
+        assertTrue(result.contains(booking1));
+        assertTrue(result.contains(booking2));
+        assertEquals(2, result.size());
+    }
+
+    @Test
+    @DisplayName("Given new booking request dates overlapping with one of two pre-existing bookings, " +
+                 "when getAllBookingsOverlappingWithDates is invoked, " +
+                 "then return list with one booking")
+    void shouldReturnListOfOneBooking() {
+        //given
+        LocalDate newBookingStartDate = LocalDate.of(2023, 10, 5);
+        LocalDate newBookingEndDate = LocalDate.of(2023, 10, 15);
+
+        Booking booking1 = Booking.builder()
+                .id(1L)
+                .startDate(LocalDate.of(2023, 9, 28))
+                .endDate(LocalDate.of(2023, 10, 10))
+                .userUuid("userUuid1")
+                .carUuid("carUuid1")
+                .build();
+
+        Booking booking2 = Booking.builder()
+                .id(2L)
+                .startDate(LocalDate.of(2023, 10, 17))
+                .endDate(LocalDate.of(2023, 10, 25))
+                .userUuid("userUuid2")
+                .carUuid("carUuid2")
+                .build();
+
+        List<Booking> bookingList = List.of(booking1,
+                                            booking2);
+
+        when(bookingRepository.findAllInMonthsOfStartAndEndDateAndInBetween(Set.of(2023),
+                                                                            List.of(10),
+                                                                            newBookingStartDate.getYear(),
+                                                                            newBookingEndDate.getYear(),
+                                                                            newBookingStartDate.getMonthValue(),
+                                                                            newBookingEndDate.getMonthValue()))
+                .thenReturn(bookingList);
+
+        //when
+        List<Booking> result = bookingService.getAllBookingsOverlappingWithDates(newBookingStartDate, newBookingEndDate);
+
+        //then
+        assertTrue(result.contains(booking1));
+        assertFalse(result.contains(booking2));
+        assertEquals(1, result.size());
+    }
+
+    @Test
+    @DisplayName("Given new booking request start date overlapping with end date of one of two pre-existing bookings, " +
+                 "when getAllBookingsOverlappingWithDates is invoked, " +
+                 "then return list with one booking")
+    void shouldReturnListOfOneBooking2() {
+        //given
+        LocalDate newBookingStartDate = LocalDate.of(2023, 10, 10);
+        LocalDate newBookingEndDate = LocalDate.of(2023, 10, 15);
+
+        Booking booking1 = Booking.builder()
+                .id(1L)
+                .startDate(LocalDate.of(2023, 9, 28))
+                .endDate(LocalDate.of(2023, 10, 10))
+                .userUuid("userUuid1")
+                .carUuid("carUuid1")
+                .build();
+
+        Booking booking2 = Booking.builder()
+                .id(2L)
+                .startDate(LocalDate.of(2023, 10, 17))
+                .endDate(LocalDate.of(2023, 10, 25))
+                .userUuid("userUuid2")
+                .carUuid("carUuid2")
+                .build();
+
+        List<Booking> bookingList = List.of(booking1,
+                                            booking2);
+
+        when(bookingRepository.findAllInMonthsOfStartAndEndDateAndInBetween(Set.of(2023),
+                                                                            List.of(10),
+                                                                            newBookingStartDate.getYear(),
+                                                                            newBookingEndDate.getYear(),
+                                                                            newBookingStartDate.getMonthValue(),
+                                                                            newBookingEndDate.getMonthValue()))
+                .thenReturn(bookingList);
+
+        //when
+        List<Booking> result = bookingService.getAllBookingsOverlappingWithDates(newBookingStartDate, newBookingEndDate);
+
+        //then
+        assertTrue(result.contains(booking1));
+        assertFalse(result.contains(booking2));
+        assertEquals(1, result.size());
+    }
+
+    @Test
+    @DisplayName("Given new booking request start date overlapping with start date of one of two pre-existing bookings, " +
+                 "when getAllBookingsOverlappingWithDates is invoked, " +
+                 "then return list with one booking")
+    void shouldReturnListOfOneBooking3() {
+        //given
+        LocalDate newBookingStartDate = LocalDate.of(2023, 10, 4);
+        LocalDate newBookingEndDate = LocalDate.of(2023, 10, 15);
+
+        Booking booking1 = Booking.builder()
+                .id(1L)
+                .startDate(LocalDate.of(2023, 10, 4))
+                .endDate(LocalDate.of(2023, 10, 10))
+                .userUuid("userUuid1")
+                .carUuid("carUuid1")
+                .build();
+
+        Booking booking2 = Booking.builder()
+                .id(2L)
+                .startDate(LocalDate.of(2023, 10, 17))
+                .endDate(LocalDate.of(2023, 10, 25))
+                .userUuid("userUuid2")
+                .carUuid("carUuid2")
+                .build();
+
+        List<Booking> bookingList = List.of(booking1,
+                                            booking2);
+
+        when(bookingRepository.findAllInMonthsOfStartAndEndDateAndInBetween(Set.of(2023),
+                                                                            List.of(10),
+                                                                            newBookingStartDate.getYear(),
+                                                                            newBookingEndDate.getYear(),
+                                                                            newBookingStartDate.getMonthValue(),
+                                                                            newBookingEndDate.getMonthValue()))
+                .thenReturn(bookingList);
+
+        //when
+        List<Booking> result = bookingService.getAllBookingsOverlappingWithDates(newBookingStartDate, newBookingEndDate);
+
+        //then
+        assertTrue(result.contains(booking1));
+        assertFalse(result.contains(booking2));
+        assertEquals(1, result.size());
+    }
+
+    @Test
+    @DisplayName("Given new booking request dates is not overlapping with any of two pre-existing bookings, " +
+                 "when getAllBookingsOverlappingWithDates is invoked, " +
+                 "then return empty list")
+    void shouldReturnEmptyList() {
+        //given
+        LocalDate newBookingStartDate = LocalDate.of(2023, 10, 11);
+        LocalDate newBookingEndDate = LocalDate.of(2023, 10, 16);
+
+        Booking booking1 = Booking.builder()
+                .id(1L)
+                .startDate(LocalDate.of(2023, 10, 2))
+                .endDate(LocalDate.of(2023, 10, 10))
+                .userUuid("userUuid1")
+                .carUuid("carUuid1")
+                .build();
+
+        Booking booking2 = Booking.builder()
+                .id(2L)
+                .startDate(LocalDate.of(2023, 10, 17))
+                .endDate(LocalDate.of(2023, 10, 25))
+                .userUuid("userUuid2")
+                .carUuid("carUuid2")
+                .build();
+
+        List<Booking> bookingList = List.of(booking1,
+                                            booking2);
+
+        when(bookingRepository.findAllInMonthsOfStartAndEndDateAndInBetween(Set.of(2023),
+                                                                            List.of(10),
+                                                                            newBookingStartDate.getYear(),
+                                                                            newBookingEndDate.getYear(),
+                                                                            newBookingStartDate.getMonthValue(),
+                                                                            newBookingEndDate.getMonthValue()))
+                .thenReturn(bookingList);
+
+        //when
+        List<Booking> result = bookingService.getAllBookingsOverlappingWithDates(newBookingStartDate, newBookingEndDate);
+
+        //then
+        assertEquals(0, result.size());
+    }
+
+    @Test
+    @DisplayName("Given new booking request end date is overlapping with start date of one of two pre-existing bookings, " +
+                 "when getAllBookingsOverlappingWithDates is invoked, " +
+                 "then return list with one booking")
+    void shouldReturnListOfOneBooking4() {
+        //given
+        LocalDate newBookingStartDate = LocalDate.of(2023, 10, 11);
+        LocalDate newBookingEndDate = LocalDate.of(2023, 10, 15);
+
+        Booking booking1 = Booking.builder()
+                .id(1L)
+                .startDate(LocalDate.of(2023, 10, 4))
+                .endDate(LocalDate.of(2023, 10, 10))
+                .userUuid("userUuid1")
+                .carUuid("carUuid1")
+                .build();
+
+        Booking booking2 = Booking.builder()
+                .id(2L)
+                .startDate(LocalDate.of(2023, 10, 15))
+                .endDate(LocalDate.of(2023, 10, 25))
+                .userUuid("userUuid2")
+                .carUuid("carUuid2")
+                .build();
+
+        List<Booking> bookingList = List.of(booking1,
+                                            booking2);
+
+        when(bookingRepository.findAllInMonthsOfStartAndEndDateAndInBetween(Set.of(2023),
+                                                                            List.of(10),
+                                                                            newBookingStartDate.getYear(),
+                                                                            newBookingEndDate.getYear(),
+                                                                            newBookingStartDate.getMonthValue(),
+                                                                            newBookingEndDate.getMonthValue()))
+                .thenReturn(bookingList);
+
+        //when
+        List<Booking> result = bookingService.getAllBookingsOverlappingWithDates(newBookingStartDate, newBookingEndDate);
+
+        //then
+        assertFalse(result.contains(booking1));
+        assertTrue(result.contains(booking2));
+        assertEquals(1, result.size());
+    }
+
+    @Test
+    @DisplayName("Given new booking request is overlapping with two pre-existing bookings, " +
+                 "when getAllBookingsOverlappingWithDates is invoked, " +
+                 "then return list with one booking")
+    void shouldReturnListOfTwoBooking() {
+        //given
+        LocalDate newBookingStartDate = LocalDate.of(2023, 9, 28);
+        LocalDate newBookingEndDate = LocalDate.of(2023, 10, 18);
+
+        Booking booking1 = Booking.builder()
+                .id(1L)
+                .startDate(LocalDate.of(2023, 10, 4))
+                .endDate(LocalDate.of(2023, 10, 10))
+                .userUuid("userUuid1")
+                .carUuid("carUuid1")
+                .build();
+
+        Booking booking2 = Booking.builder()
+                .id(2L)
+                .startDate(LocalDate.of(2023, 10, 15))
+                .endDate(LocalDate.of(2023, 10, 25))
+                .userUuid("userUuid2")
+                .carUuid("carUuid2")
+                .build();
+
+        List<Booking> bookingList = List.of(booking1,
+                                            booking2);
+
+        when(bookingRepository.findAllInMonthsOfStartAndEndDateAndInBetween(Set.of(2023),
+                                                                            List.of(9, 10),
+                                                                            newBookingStartDate.getYear(),
+                                                                            newBookingEndDate.getYear(),
+                                                                            newBookingStartDate.getMonthValue(),
+                                                                            newBookingEndDate.getMonthValue()))
+                .thenReturn(bookingList);
+
+        //when
+        List<Booking> result = bookingService.getAllBookingsOverlappingWithDates(newBookingStartDate, newBookingEndDate);
 
         //then
         assertTrue(result.contains(booking1));
